d60e6e39a9d1be4af338aa2c6eee6a3aa882bfa9
Aravind <aravindreddy8900@gmail.com>
2026-01-01 23:24:41 +0530

    done1

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:3000");
+const socket = io();
 
 const messageform = document.getElementById("send-container");
 const messageinput = document.getElementById("message-input");
